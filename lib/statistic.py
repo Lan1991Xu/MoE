ec9d18c291ee38062255f7f182ecb6c403a44306
mayorx <chenliangyux@gmail.com>
2018-05-21 23:00:58 +0800

    statistic bugfix

--- lib/statistic.py
+++ lib/statistic.py
@@ -20,10 +20,20 @@ class AverageMeter(object):
 def correct_count(output, target, reduce=True):
     _, pred = torch.topk(output, 1, 1, True, True)
 
-    ans = pred == target.view(pred.shape)
+    ans = (pred == target.view(pred.shape)).long()
     if reduce:
         ans = ans.sum()
     return ans
 
 
-# def oracle_count():
+def oracle_count(outputs, target, reduce=True):
+    correct_detail = []
+    for name in outputs:
+        output = outputs[name]
+        correct = correct_count(output, target, reduce=False)
+        correct_detail.append(correct.view(-1, 1))
+    correct_detail = torch.cat(correct_detail, dim=1)
+    ans = (correct_detail.sum(dim=1) > 0).long()
+    if reduce:
+        ans = ans.sum()
+    return ans
