e010347e3d5867cd89a03f6ee674aeeb31f9d176
mayorx <chenliangyux@gmail.com>
2018-05-22 15:09:02 +0800

    dataset pool

--- lib/dataset.py
+++ lib/dataset.py
@@ -6,11 +6,17 @@ class dataset():
     def __init__(self, dataset):
         self.dataset = dataset
         self.normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
-        if not (self.dataset in set(['cifar10', 'cifar100', 'svhn']) ):
+        self.trainloader_pool = {}
+        self.testloader_pool = {}
+        if not (self.dataset in set(['cifar10', 'cifar100', 'svhn'])):
             print('dataset {} is not supported.'.format(self.dataset))
             raise RuntimeError
 
     def trainloader(self, batch_size=128, data_augment=True):
+        key = '{}_{}'.format(batch_size, data_augment)
+        if self.trainloader_pool.get(key):
+            return self.trainloader_pool.get(key)
+
         transform_list = []
         if data_augment:
             transform_list.append(transforms.RandomCrop(32, padding=4))
@@ -40,9 +46,14 @@ class dataset():
             raise RuntimeError
 
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+        self.trainloader_pool[key] = train_loader
         return train_loader
 
     def testloader(self, batch_size=100):
+        key = str(batch_size)
+        if self.testloader_pool.get(key):
+            return self.testloader_pool.get(key)
+
         transform_list = []
         transform_list.append(transforms.ToTensor())
         transform_list.append(self.normalize)
@@ -71,6 +82,7 @@ class dataset():
             raise RuntimeError
 
         test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+        self.testloader_pool[key] = test_loader
         return test_loader
 
     def num_classes(self):
